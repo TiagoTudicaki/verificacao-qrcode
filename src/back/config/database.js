730e50f552f60c1ea20a0ad367c72652fa206384
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-05 19:11:54 -0300

    chore: adiciona configurações do banco

--- src/back/config/database.js
+++ src/back/config/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
+// cria conexão do banco com as crendênciais.
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,    
-  port: process.env.DB_PORT || 3006,  
+  port: process.env.DB_PORT || 3306,  
 });
 
 module.exports = pool;
