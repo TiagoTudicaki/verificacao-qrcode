48f7c77db11cf8516a5001c8aecd26342f201557
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-06 02:42:34 -0300

    feat: adiciona a rota de pickup

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const pickupRoute = require("./src/back/routes/pickupRoute");
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "src/front/public")));
+
+app.use(pickupRoute);
 module.exports = app;
+
