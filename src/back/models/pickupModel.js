545421724ac486568d2923352f195f71a00f8bda
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-06 05:18:41 -0300

    feat: adiciona a função verify que consulta e executa a atualização na tabela pickups

--- src/back/models/pickupModel.js
+++ src/back/models/pickupModel.js
@@ -1,11 +1,34 @@
 const db = require("../config/database");
 
+
 const pickupModel = {
     async create(orderNumber, clientToken, equipmentToken){
         const sql = "INSERT INTO pickups (order_number, client_token, equipment_token)VALUES(?, ?, ?)";
         const[result] = await db.query(sql,[orderNumber, clientToken, equipmentToken]);
         return result;
+    },
+
+    async verify(clientToken, equipmentToken){
+        const [rows] = await db.execute(
+            "SELECT * FROM pickups WHERE client_token = ? AND equipment_token = ?",
+            [clientToken, equipmentToken]
+        );
+
+        return rows[0];
+
+    
+    },
+
+    async update(id, newStatus, checkedAt, confirmationMethod ){
+
+        const [confirmedPickup] = await db.query(
+            "UPDATE pickups SET status = ?, checked_at = ?, confirmation_method = ? WHERE id = ?",[newStatus, checkedAt, confirmationMethod, id]
+        );
+
+        return confirmedPickup;
     }
+
+
 }
 
 module.exports = pickupModel;
