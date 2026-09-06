4d509fbd19fc941354a3819729e02b1aea89fc89
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-06 03:08:14 -0300

    feat: adiciona função verify em controller de pickup

--- src/back/controllers/pickupController.js
+++ src/back/controllers/pickupController.js
@@ -17,7 +17,25 @@ const pickupController = {
         }catch(erro){
             return res.status(400).json(erro);
         }
+    },
+
+    async verify(req, res){
+        try{
+            if(Object.keys(req.body).length < 1){
+                const erro = new Error("Os campos são obrigatórios");
+                throw erro;
+            }
+
+            const {client_token, equipment_token} = req.body;
+
+            const verifyPickup = await pickupService.verify(client_token, equipment_token);
+            return res.status(200).json(verifyPickup);
+        }catch(erro){
+            return res.status(400).json({message:erro.message});
+        }
     }
 }
+
+
 
 module.exports = pickupController;
