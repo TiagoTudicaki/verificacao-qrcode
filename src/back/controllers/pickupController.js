91fecaad208eb7f03ae7f0d5a658322908ca5f22
Tiago <tiago.h.tudicaki@gmail.com>
2026-09-05 23:21:23 -0300

    feat: adiciona função create no controller de pickup

---
+++ src/back/controllers/pickupController.js
@@ -0,0 +1,21 @@
+const pickupService = require("../services/pickupService");
+
+const pickupController = {
+    async create(req, res){
+        try{
+           
+        if(Object.keys(req.body).length === 0){
+            const erro = new Error("Requisição vazia");
+            throw erro;
+        }
+
+        const {orderNumber} = req.body;
+
+        const newPickup = await pickupService.create(orderNumber);
+        return res.status(201).json(newPickup);
+
+        }catch(erro){
+            return res.status(400).json(erro);
+        }
+    }
+}
